894ed41ac62ef96e81764808089e72d096789c26
cow785785 <zcbm030199@gmail.com>
2023-04-19 11:33:22 +0800

    findprime

---
+++ index.js
@@ -0,0 +1,25 @@
+const textarea = document.getElementById("textarea");
+const input = document.getElementById("num");
+const button = document.getElementById("btn");
+function isPrime(n) {
+    if (n <= 1) {
+      return false;
+    }
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  button.addEventListener("click", function () {
+    const num = parseInt(input.value);
+    let primes = [];
+    for (let i = 2; i < num; i++) {
+      if (isPrime(i)) {
+        primes.push(i);
+      }
+    }
+    textarea.textContent ="小於" + num + "的質數有：" + primes.join(", ");
+  });
